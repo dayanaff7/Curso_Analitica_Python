a86eda0f91a51f98532503c7249b5067f580b0ad
Dayanna Flores <dayanna@gmail.com>
2024-04-16 00:10:23 -0400

    Muestra el valor absoluto según el porcentaje y el total

---
+++ valor_absoluto.py
@@ -0,0 +1,10 @@
+total_personas = 1500
+porcentaje = 0.15
+
+# Calcular el 25% del total
+valor_porcentaje = total_personas * porcentaje
+
+# Tomar el valor absoluto
+valor_absoluto = abs(valor_porcentaje)
+
+print("El valor absoluto del 25% de 1500 personas es:", valor_absoluto)
